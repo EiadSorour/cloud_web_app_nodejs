03383d98f52f1a42e73ece0cf7574b3b55cfeed5
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-05-05 17:22:41 +0300

    connected_to_postgres_database

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const { Sequelize } = require('sequelize');
 
 const app = express();
+
+const sequelize = new Sequelize('Student_Data', 'postgres', 'eiad1422003', {
+    host: "localhost",
+    dialect: "postgres",
+    port: 5432
+});
+
+sequelize.authenticate()
+    .then(()=>{
+        console.log("Connected to postgres database");
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
 
 app.get("/" , (req,res)=>{
     res.send("<h1>Hello from express</h1>");
